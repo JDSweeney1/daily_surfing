cbab0a43adba0bd7a6078ca0f8e9dd23d1146286
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-05 21:33:26 -0500

    added even more functionality to Cli

--- lib/daily_surfing/cli.rb
+++ lib/daily_surfing/cli.rb
@@ -8,38 +8,55 @@ class DailySurfing::Cli
 
     #you will also be able to select individual attributes for each region
     puts "welcome to Daily Surfing!"
-    puts "Get your 3 day forecast for the entire east coast of Florida by typing 'all'"
-    puts "Or select one of four regions using their corresponding number.
-     1. North Florida
-     2. Central Florida
-     3. Treasure Coast - Palm Beach
-     4. Boward - Miami Dade"
 
-     input = gets.strip
-
-     if input == "all"
-       #will print out all data for all regions.
-       #insert fake data next
-     if input == "1"
-       #will ask if they want all information or select report
-       #will use if statements to deliver report. possible loop until exit to stay in region
-       #fake surf_data
-     if input == "1"
-       #will ask if they want all information or select report
-       #will use if statements to deliver report. possible loop until exit to stay in region
-       #fake surf_data
-     if input == "1"
-       #will ask if they want all information or select report
-       #will use if statements to deliver report. possible loop until exit to stay in region
-       #fake surf_data
-     if input == "1"
-       #will ask if they want all information or select report
-       #will use if statements to deliver report. possible loop until exit to stay in region
-       #fake surf_data
-     else
-       #have some sort of error message.
-       #restart at top.
+    loop do
+      puts ""
+      puts "Get your 3 day forecast for the entire east coast of Florida by typing 'all'"
+      puts "Or select one of four regions using their corresponding number.
+       1. North Florida
+       2. Central Florida
+       3. Treasure Coast - Palm Beach
+       4. Boward - Miami Dade"
+      puts "         -----Type 'exit' to exit-----"
+       input = gets.strip
+       if input == "all"
+         #will print out all data for all regions.
+         #insert fake data next
+         puts ""
+         puts "here is all"
+         #will have an ask feature that asks if they want to go back to main menu or exit
+       elsif input == "1"
+         #will ask if they want all information or select report
+         #will use if statements to deliver report. possible loop until exit to stay in region
+         #fake surf_data
+         puts ""
+         puts "here is 1"
+       elsif input == "2"
+         #will ask if they want all information or select report
+         #will use if statements to deliver report. possible loop until exit to stay in region
+         #fake surf_data
+         puts ""
+         puts "here is 2"
+       elsif input == "3"
+         #will ask if they want all information or select report
+         #will use if statements to deliver report. possible loop until exit to stay in region
+         #fake surf_data
+         puts ""
+         puts "here is 3"
+      elsif input == "4"
+         #will ask if they want all information or select report
+         #will use if statements to deliver report. possible loop until exit to stay in region
+         #fake surf_data
+         puts ""
+         puts "here is 4"
+       elsif input == "exit"
+         exit
+       else
+         #have some sort of error message.
+         #restart at top.
+         puts "here is wrong"
+       end
      end
-
-  end
+     #exit
+ end
 end
