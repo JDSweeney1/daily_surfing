a99b61bc9e1456c9abc299c20feb692b13b403e0
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-06 18:31:11 -0500

    dound the Nokogiri codes needed for the scraper class

--- lib/daily_surfing/scraper.rb
+++ lib/daily_surfing/scraper.rb
@@ -1,11 +1,44 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
 class Scraper
   include Attributes
 
   def initialize(url)
+    #html = File.read("url")
+    surf_report = Nokogiri::HTML(open(url))
+    binding.pry
     #fake surf_data
-    #open html and nokogiri go here
+    #date: surf_report.css("div.day-slider-container div div span").first.text
+    data = surf_report.css("div.day-slider-container div div span").collect{|t| t.text}.delete_if{|k| k.include?("Kurt")}
+    #data[0] = first day  data[1] = brief description data[2] = wind description
+    #data[3] = second day data[4] = brief description data[5] = wind description
+    #data[6] = third day  data[7] = brief description data[8] = wind description
+    #data[9] = first day weather data[10].gesub('\u00B0', "") = first day temp
+
+    #weather = surf_report.css("div.day-slider-container div div strong").collect{|w| w.text}
+    #weather[0] = day 1 weather[1] = day 2 weather [2] = day 3
+
+=begin
+    ["Wednesday - 12/06",
+ "ankle to knee high",
+ "Small SE windswell with SE winds.  ",
+ "Thursday - 12/07",
+ "Flat",
+ "Minimal windswell traces. Light winds.  ",
+ "Friday - 12/08",
+ "Flat",
+ "Minimal traces. Light winds.  ",
+ "scattered showers",
+ "71-83\u00B0 F",
+ "sunny",
+ "70-83\u00B0 F",
+ "scattered showers",
+ "71-82\u00B0 F"]
+=end
     @name = "broward_miami_dade"
-    @date = "12/05"
+    @date = data[0]
     @condition = "fair"
     @wave_height = "2-3ft"
     @wind = "LP LP LP"
