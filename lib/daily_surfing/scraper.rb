aaeaf978169de55d9a933be82ada9258948e94ec
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-06 19:38:41 -0500

    fixed scraper bug

--- lib/daily_surfing/scraper.rb
+++ lib/daily_surfing/scraper.rb
@@ -12,7 +12,6 @@ class Scraper
 
     self.wave_height = surf_report.css("div.day-slider-container h1").collect{|h| h.text}
 
-
     data = surf_report.css("div.day-slider-container div div span").collect{|t| t.text}.delete_if{|k| k.include?("Kurt")}
     self.date = []
     self.date << data[0]
@@ -37,21 +36,21 @@ class Scraper
 
   def self.broward_miami_dade
     scraper = self.new("http://www.surfline.com/surf-forecasts/florida/broward---miami-dade_121124/")
-    Surf_Data.new("Broward/Miami/Dade", scraper.date, scraper.condition, scraper.wave_height, scraper.wind, scraper.weather, scrape.weather_temp)
+    Surf_Data.new("Broward/Miami/Dade", scraper.date, scraper.condition, scraper.wave_height, scraper.wind, scraper.weather, scraper.weather_temp)
   end
 
   def self.palm_beach
     scraper = self.new("http://www.surfline.com/surf-forecasts/florida/treasure-coast---palm-beach_2155/")
-    Surf_Data.new("Palm Beach", scraper.date, scraper.condition, scraper.wave_height, scraper.wind, scraper.weather, scrape.weather_temp)
+    Surf_Data.new("Palm Beach", scraper.date, scraper.condition, scraper.wave_height, scraper.wind, scraper.weather, scraper.weather_temp)
   end
 
   def self.cental_florida
     scraper = self.new("http://www.surfline.com/surf-forecasts/florida/central-florida_2154/")
-    Surf_Data.new("Central Florida", scraper.date, scraper.condition, scraper.wave_height, scraper.wind, scraper.weather, scrape.weather_temp)
+    Surf_Data.new("Central Florida", scraper.date, scraper.condition, scraper.wave_height, scraper.wind, scraper.weather, scraper.weather_temp)
   end
 
   def self.north_florida
     scraper = self.new("http://www.surfline.com/surf-forecasts/florida/north-florida_2153/")
-    Surf_Data.new("North Florida", scraper.date, scraper.condition, scraper.wave_height, scraper.wind, scraper.weather, scrape.weather_temp)
+    Surf_Data.new("North Florida", scraper.date, scraper.condition, scraper.wave_height, scraper.wind, scraper.weather, scraper.weather_temp)
   end
 end
